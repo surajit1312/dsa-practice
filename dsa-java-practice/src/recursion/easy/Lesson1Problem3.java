f1576092fc6d5398f6dfa731a3d0500642590d34
surajitrana1985 <surajit.cool@gmail.com>
2023-08-30 19:08:19 +0530

    [fix]: Formatted the files and changed package name

---
+++ dsa-java-practice/src/recursion/easy/Lesson1Problem3.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package recursion.easy;
+
+/**
+ * Print value of factorial of a given number using recursion
+ */
+public class Lesson1Problem3 {
+
+	private static double calculateFactorial(int n) {
+		if (n == 1 || n == 0) {
+			return 1;
+		}
+		return n * calculateFactorial(n - 1);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n = 100;
+		double factorial = calculateFactorial(n);
+		System.out.println("Factorial of " + n + " is : " + factorial);
+	}
+}
