3a999b0e9c67740969b5f0a1b6efeff32b49a993
surajit1312 <surajit1602@gmail.com>
2023-09-09 01:21:35 +0530

    feat[recursion]: sum of 1st n natural numbers

---
+++ dsa-js-practice/recursion/p5-sum-of-n-numbers.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/**
+ * Print sum of 1st n natural numbers
+ * Time Complexity - 
+ * Space Complexity - 
+ * @param {*} n 
+ * @returns 
+ */
+function sumNaturalNumbers(n) {
+    let sum = 0;
+    sumRecursiveUsingParams(n, sum);
+    const sumOfN = sumNum(n);
+    console.log("(Using recursive function) Sum of " + n + " natural numbers is :", sumOfN);
+}
+
+/**
+ * Finding sum using recursion parameters
+ * @param {*} n 
+ * @param {*} sum 
+ * @returns 
+ */
+function sumRecursiveUsingParams(n, sum) {
+    if (n < 0) {
+        return;
+    }
+    if (n === 0) {
+        console.log("Sum of " + n + " natural numbers is :", sum);
+        return;
+    }
+    sum += n;
+    sumRecursiveUsingParams(n - 1, sum);
+}
+
+/**
+ * Finding sum using recursion parameters
+ * @param {*} n 
+ * @param {*} sum 
+ * @returns 
+ */
+function sumNum(n) {
+    if (n < 1) {
+        return 0;
+    }
+    return n + sumNum(n - 1);
+}
+
+console.log("================");
+const input1 = 5;
+sumNaturalNumbers(input1);
+
+console.log("================");
+const input2 = 10;
+sumNaturalNumbers(input2);
+
+console.log("================");
+const input3 = -2;
+sumNaturalNumbers(input3);
