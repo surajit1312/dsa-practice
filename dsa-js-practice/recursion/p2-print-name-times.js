33f4dc57cd5d0c98f49c531258e9c15041cc6fb9
surajit1312 <surajit1602@gmail.com>
2023-09-08 21:06:46 +0530

    feat[recursion]: print name n times

---
+++ dsa-js-practice/recursion/p2-print-name-times.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function printName(name, n) {
+    return printRecursive(name, n, 1);
+}
+
+function printRecursive(name, n, count) {
+    // base case
+    if (n < 0 || count > n) {
+        return;
+    }
+    console.log(name);
+    return printRecursive(name, n, count + 1);
+}
+
+const input1 = "Surajit";
+const output1 = printName(input1, 5);
+
+const input2 = "Hello";
+const output2 = printName(input2, 10);
+
+const input3 = "Zero";
+const output3 = printName(input3, -1);
