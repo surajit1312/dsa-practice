70724a0eb30849a6791b4b4861c9b3a447e2c1d3
surajit1312 <surajit1602@gmail.com>
2023-09-10 22:20:47 +0530

    fix[sorting]: bubble sort optimized for best case

--- dsa-js-practice/sorting/p2-bubble-sort.js
+++ dsa-js-practice/sorting/p2-bubble-sort.js
@@ -13,12 +13,17 @@ function bubbleSort(arr) {
     // loop is the step which would start from 0 to (n - 2)th term
     const n = arr.length;
     for (let i = n - 1; i >= 1; i--) {
+        let hasSwapped = false;
         for (let j = 0; j <= i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                hasSwapped = true;
             }
+        }
+        if (!hasSwapped) {
+            break;
         }
     }
     return arr;
